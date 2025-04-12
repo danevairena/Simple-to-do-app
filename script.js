becb9d065dcd2ab013e739270312bc14e79c1faf
Irena Daneva <de.luxe9997@gmail.com>
2025-04-12 20:24:05 +0300

    update

--- script.js
+++ script.js
@@ -8,32 +8,3 @@ function CreateToDoData() {
 
     }
 }
-
-
-
-/*let todoList = [];
-
-//Save tot list to localstorage
-const saveTodo = (todoList) => {
-    if (localStorage && todoList.length)
-        //LocalStorage can only store strings
-        //JSON.stringify() method converts the value into a string representation before sending it to local storage.
-        localStorage.setItem("todoList", JSON.stringify(todoList));
-  };
-
-  const getTodo = () => {
-    let todoList;
-    if (localStorage && localStorage.getItem("todoList")) {
-        //JSON.parse() method converts the value from its string representation back to the original form
-      todoList = JSON.parse(localStorage.getItem("todoList"));
-    }
-    return todoList || [];
-  };
-
-function addTask() {
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    var text = document.getElementById("task").value;
-    li.textContent = text; // Setting the text
-    ul.appendChild(li); // Appending new li to ul
-}*/
